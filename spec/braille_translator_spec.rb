7288f1455e3d23dc4407fbfc0599815977018cc6
Will Wang <willy.j.wang@gmail.com>
2022-09-24 17:40:46 -0500

    Write tests for braille_translator class

--- spec/braille_translator_spec.rb
+++ spec/braille_translator_spec.rb
@@ -13,4 +13,10 @@ RSpec.describe BrailleTranslator do
       expect(translator.lowercase[:a]).to eq(["0.", "..", ".."])
     end
   end
+  describe '#char_to_braille' do
+    it "can return a string of text translated to braille" do
+      translator = BrailleTranslator.new
+      expect(translator.char_to_braille("hello")).to eq("0.0.0.0.0.\n00.00.0..0\n....0.0.0.")
+    end
+  end
 end
