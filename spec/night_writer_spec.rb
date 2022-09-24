f8b6787829377750d3836cd74508ba793ed0eeb0
Will Wang <willy.j.wang@gmail.com>
2022-09-24 18:20:50 -0500

    Modify night_writer_spec with change from get_file_char_length to get_file_text

--- spec/night_writer_spec.rb
+++ spec/night_writer_spec.rb
@@ -10,23 +10,24 @@ RSpec.describe NightWriter do
   end
   describe '#print_confirmation' do
     it "can print out a confirmation message" do
+      allow(File).to receive(:readlines).and_return(["hello world\n"])
       night_writer = NightWriter.new("message.txt", "braille.txt")
-      expect {night_writer.print_confirmation}.to output("Created 'braille.txt' containing 12 characters\n").to_stdout
+      expect {night_writer.print_confirmation}.to output("Created 'braille.txt' containing 11 characters\n").to_stdout
     end
   end
-  describe '#get_file_char_length' do
+  describe '#get_file_text' do
     it "can return the read file's character length" do
       allow(File).to receive(:readlines).and_return(["hello world\n"])
       night_writer = NightWriter.new("message.txt", "braille.txt")
-      expect(night_writer.get_file_char_length).to eq(12)
+      expect(night_writer.get_file_text).to eq("hello world")
     end
   end
   describe '#write_to_file' do
+    #WIP
     it "can write a braille translation to a file" do
       allow(File).to receive(:readlines).and_return(["hello world\n"])
       night_writer = NightWriter.new("message.txt", "braille.txt")
-      expect(night_writer.write_to_file).to eq()
-    #?????
+      expect(night_writer.write_to_file).to eq(nil)
     end
   end
 end
