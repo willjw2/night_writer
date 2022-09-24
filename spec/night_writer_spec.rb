563d91dd135c1c396ecac562086301302b8d7cdc
Will Wang <willy.j.wang@gmail.com>
2022-09-24 14:56:33 -0500

    Modify tests for runner file inclusion

--- spec/night_writer_spec.rb
+++ spec/night_writer_spec.rb
@@ -4,15 +4,22 @@ require './lib/night_writer'
 RSpec.describe NightWriter do
   describe '#initialize' do
     it "exists" do
-      night_writer = NightWriter.new
+      night_writer = NightWriter.new("message.txt", "braille.txt")
       expect(night_writer).to be_an_instance_of(NightWriter)
     end
   end
   describe '#print_confirmation' do
+    #WIP
     it "can print out a confirmation message" do
-      night_writer = NightWriter.new
-      allow(NightWriter).to receive(:ARGV).and_return(["message.txt", "braille.txt"])
-      expect(night_writer.print_confirmation).to eq("Created 'braille.txt' containing 0 characters")
+      night_writer = NightWriter.new("message.txt", "braille.txt")
+      expect {night_writer.print_confirmation}.to output("Created 'braille.txt' containing 12 characters\n").to_stdout
+    end
+  end
+  describe '#get_file_char_length' do
+    it "can return the read file's character length" do
+      allow(File).to receive(:readlines).and_return(["hello world\n"])
+      night_writer = NightWriter.new("message.txt", "braille.txt")
+      expect(night_writer.get_file_char_length).to eq(12)
     end
   end
 end
