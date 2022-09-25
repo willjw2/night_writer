64d9764ff56c310734d27bd15081e13e7462da97
Will Wang <willy.j.wang@gmail.com>
2022-09-25 12:29:55 -0500

    Rewrite char_to_braille to consider iteration 4/character limit, using helper method create_braille_string

--- lib/braille_translator.rb
+++ lib/braille_translator.rb
@@ -58,25 +58,110 @@ class BrailleTranslator
       "Y" => ["..00", "...0", ".000"],
       "Z" => ["..0.", "...0", ".000"],
     }
+    #WIP
     @contractions = {
-
+      " a " => ["..0...", "......", "......"],
+      " but " => ["..0...", "..0...", "......"],
+      " can " => ["..00..", "......", "......"]
     }
-
+    @numbers = {
+      "1" => ["0.", "..", ".."],
+      "2" => ["0.", "0.", ".."],
+      "3" => ["00", "..", ".."],
+      "4" => ["00", ".0", ".."],
+      "5" => ["0.", ".0", ".."],
+      "6" => ["00", "0.", ".."],
+      "7" => ["00", "00", ".."],
+      "8" => ["0.", "00", ".."],
+      "9" => [".0", "0.", ".."],
+      "0" => [".0", "00", ".."],
+      "#" => [".0", ".0", "00"]
+    }
   end
 
   def char_to_braille(text)
+    braille_lines = {1 => "", 2 => "", 3 => ""}
+    braille_char_num = 0
+
+    text.each_char.with_index do |char, index|
+      if char.ord.between?(97, 122) || char.ord == 32
+        braille_lines[1] += @lowercase[char][0]
+        braille_lines[2] += @lowercase[char][1]
+        braille_lines[3] += @lowercase[char][2]
+        braille_char_num += 1
+      # elsif char.ord.between?(65, 90)
+      #   braille_lines[1] += @uppercase[char][0]
+      #   braille_lines[2] += @uppercase[char][1]
+      #   braille_lines[3] += @uppercase[char][2]
+      #   braille_char_num += 2
+      # elsif char.ord.between?(48, 57)
+      #   if index - 1 >= 0 && text[index-1].ord.between?(48, 57)
+      #     braille_lines[1] += @numbers[char][0]
+      #     braille_lines[2] += @numbers[char][1]
+      #     braille_lines[3] += @numbers[char][2]
+      #     braille_char_num += 1
+      #   else
+      #     braille_lines[1] += @numbers["#"][0]
+      #     braille_lines[1] += @numbers["#"][1]
+      #     braille_lines[1] += @numbers["#"][2]
+      #     braille_lines[1] += @numbers[char][0]
+      #     braille_lines[2] += @numbers[char][1]
+      #     braille_lines[3] += @numbers[char][2]
+      #     braille_char_num += 2
+      #   end
+      end
+    end
+    create_braille_string(braille_lines, braille_char_num)
+  end
+
+  def create_braille_string(braille_lines, braille_char_num)
     braille_string = String.new
-    text.each_char do |char|
-      braille_string += @lowercase[char.to_sym][0]
-    end
-    braille_string += "\n"
-    text.each_char do |char|
-      braille_string += @lowercase[char.to_sym][1]
-    end
-    braille_string += "\n"
-    text.each_char do |char|
-      braille_string += @lowercase[char.to_sym][2]
+    if braille_char_num > 40
+      #WIP
+      rows = braille_char_num / 40
+      extra_chars = braille_char_num % 40
+      rows.times do |row_num|
+        start_indice = row_num * 80
+        end_indice = ((row_num + 1) * 80) - 1
+        braille_string += braille_lines[1][start_indice..end_indice]
+        braille_string += "\n"
+        braille_string += braille_lines[2][start_indice..end_indice]
+        braille_string += "\n"
+        braille_string += braille_lines[3][start_indice..end_indice]
+        braille_string += "\n"
+      end
+      start_indice = (rows * 80)
+      braille_string += braille_lines[1][start_indice..]
+      braille_string += "\n"
+      braille_string += braille_lines[2][start_indice..]
+      braille_string += "\n"
+      braille_string += braille_lines[3][start_indice..]
+    else
+      braille_string += braille_lines[1]
+      braille_string += "\n"
+      braille_string += braille_lines[2]
+      braille_string += "\n"
+      braille_string += braille_lines[3]
     end
     braille_string
   end
 end
+
+# translator = BrailleTranslator.new
+# require "pry"; binding.pry
+
+#def char_to_braille
+# braille_string = String.new
+# text.each_char do |char|
+#   braille_string += @lowercase[char.to_sym][0]
+# end
+# braille_string += "\n"
+# text.each_char do |char|
+#   braille_string += @lowercase[char.to_sym][1]
+# end
+# braille_string += "\n"
+# text.each_char do |char|
+#   braille_string += @lowercase[char.to_sym][2]
+# end
+# braille_string
+#end
