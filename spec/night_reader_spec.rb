2ab8ea191b3bf93ecdfee781c26d5cfa59897bd2
Will Wang <willy.j.wang@gmail.com>
2022-09-25 15:15:21 -0500

    Write test for get_file_characters

--- spec/night_reader_spec.rb
+++ spec/night_reader_spec.rb
@@ -12,7 +12,17 @@ RSpec.describe NightReader do
     it "can print a confirmation message" do
       allow(File).to receive(:readlines).and_return(["0.0.0.0.0.\n00.00.0..0\n....0.0.0."])
       night_reader = NightReader.new("braille.txt", "original_message.txt")
-      expect {night_writer.print_confirmation}.to output("Created 'original_message.txt' containing 5 characters\n").to_stdout
+      expect {night_reader.print_confirmation}.to output("Created 'original_message.txt' containing 5 characters\n").to_stdout
+    end
+  end
+  describe '#get_file_braille' do
+    
+  end
+  describe '#get_file_characters' do
+    it "can return the number of braille characters in a file" do
+      allow(File).to receive(:readlines).and_return(["0.0.0.0.0.\n00.00.0..0\n....0.0.0."])
+      night_reader = NightReader.new("braille.txt", "original_message.txt")
+      expect(night_reader.get_file_characters).to eq(5)
     end
   end
 end
